ba8ffb15bb9642c00415700682d9df445da94ac4
AntI300000 <anthonyi@udel.edu>
2025-03-01 18:51:28 -0500

    making api calls

--- backend/mysite/PassWarrior/views.py
+++ backend/mysite/PassWarrior/views.py
@@ -1,10 +1,19 @@
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse
+from dotenv import load_dotenv
+import os
 # Create your views here.
+
+load_dotenv()    
+api_key = os.getenv("API_KEY")
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the password page.")
 
 def display(request):
+    x = requests.get("https://haveibeenpwned.com/api/v3/breachedaccount/io311@comcast.net", headers = {"user-agent": "PassWarrior", "hibp-api-key": api_key})
+    print(x.text)
     return render(request, "Passwarrior.html")
-
+    
