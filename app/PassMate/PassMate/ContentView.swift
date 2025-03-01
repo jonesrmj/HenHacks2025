1735f4983404b898b63170dab904a3166f715ea4
jonesrmj <jones.rmj@gmail.com>
2025-03-01 15:10:35 -0500

    Created App Homepage Layout

--- app/PassMate/PassMate/ContentView.swift
+++ app/PassMate/PassMate/ContentView.swift
@@ -6,83 +6,97 @@
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
+  @Environment(\.horizontalSizeClass) var horizontalSizeClass
+  @Environment(\.colorScheme) var colorScheme
+  
+  @State var password: String = ""
+  
+  var body: some View {
+    GeometryReader { geometry in
+      let isLandscape = geometry.size.width > geometry.size.height
+      let isWideScreen = horizontalSizeClass == .regular // Detects iPad/Mac
+      
+      ScrollView {
+        if isLandscape || isWideScreen {
+          // iPad/Mac
+          
+        } else {
+          // iPhone
+          VStack {
+            ZStack {
+              RoundedRectangle(cornerRadius: 20)
+                .fill(Color("systemBackground"))
+                .overlay(
+                  RoundedRectangle(cornerRadius: 20)
+                    .stroke(colorScheme == .dark ? Color.white.opacity(0.2) : Color.gray.opacity(0.3), lineWidth: 1)
+                )
+                .shadow(color: Color.black.opacity(colorScheme == .dark ? 0.5 : 0.1), radius: 10, x: 0, y: 5)
+              
+              VStack {
+                Image("PassMate Logo")
+                  .resizable()
+                  .frame(width: 124, height: 124)
+                  .shadow(color: Color.black.opacity(colorScheme == .dark ? 0.5 : 0.1), radius: 10, x: 0, y: 5)
+                  .padding(.top, 20.0)
+                
+                Text("PassMate")
+                  .font(.title)
+                  .bold()
+                  .padding(.top, 10.0)
+                
+                Text("Your trusted companion for Password Security!")
+                  .font(.title3)
+                  .padding(.top, 0.25)
+                  .padding(.bottom, 20.0)
+              }
+              .multilineTextAlignment(.center)
+            }
+            .padding(.all, 20.0)
+            
+            ZStack {
+              RoundedRectangle(cornerRadius: 20)
+                .fill(Color("systemBackground"))
+                .overlay(
+                  RoundedRectangle(cornerRadius: 20)
+                    .stroke(colorScheme == .dark ? Color.white.opacity(0.2) : Color.gray.opacity(0.3), lineWidth: 1)
+                )
+                .shadow(color: Color.black.opacity(colorScheme == .dark ? 0.5 : 0.1), radius: 10, x: 0, y: 5)
+              
+              VStack {
+                Text("Password Checkup")
+                  .font(.title2)
+                  .bold()
+                  .padding(.top, 20.0)
+                
+                Text("Enter a password to check its strength, verify if it has appeared in any data leaks, and receive additional feedback.")
+                  .font(.body)
+                  .padding(.all, 10.0)
+                
+                TextField("Enter a Password", text: self.$password)
+                  .padding(.leading, 10.0)
+                  .padding(.trailing, 10.0)
+                  .padding(.bottom, 10.0)
+                
+                Button {
+                  // TBD
+                } label: {
+                  Text("Analyze")
                 }
-                .onDelete(perform: deleteItems)
+                .padding(.bottom, 20.0)
+              }
+              .multilineTextAlignment(.center)
             }
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+            .padding(.leading, 20.0)
+            .padding(.trailing, 20.0)
+          }
         }
+      }
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+  }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 #Preview {
-    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+  ContentView()
 }
