57e53f10e1ca76511641f7250e51fbe9e96d6e48
AntI300000 <anthonyi@udel.edu>
2025-03-01 20:10:22 -0500

    API Urls

--- backend/mysite/mysite/urls.py
+++ backend/mysite/mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("password/", include("PassWarrior.urls")),
+    path("API/", include("API.urls")),
     path('admin/', admin.site.urls),
 ]
