3896dd05de36e8cff924d062595feac07e2d0206
AntI300000 <anthonyi@udel.edu>
2025-03-02 09:46:49 -0500

    Secret Key

--- backend/mysite/mysite/settings.py
+++ backend/mysite/mysite/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()  
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-busdm%&9h9e96-f4gkbrwwfy@sauzdy%x1lnwb0gdslvg%_c*z'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "167.172.140.119"]
 
 
 # Application definition
