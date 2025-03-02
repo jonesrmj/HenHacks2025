c1e62d4fa1c1bb9bbde4b674112670254b03a4bb
AntI300000 <anthonyi@udel.edu>
2025-03-01 20:07:16 -0500

    POST Implamentation

--- backend/mysite/API/views.py
+++ backend/mysite/API/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
 # Create your views here.
 
@@ -6,5 +8,9 @@ def CallHandler(request):
     if request.method == "POST":
         password = request.POST.get["password"]
         #make api calls here
+
+        return HttpResponse(json.dumps({"password": password}))
+    else:
+        return HttpResponse("Not a POST Request")
 
 
